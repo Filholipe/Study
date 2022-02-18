c58cca3928fed729237b23a6b17372b9efb9d0ca
Filholipe <felipegz.az@gmail.com>
2022-02-18 00:34:36 -0300

    Study

---
+++ aula.js
@@ -0,0 +1,57 @@
+// variaveis
+
+// let idade = 5;
+// console.log(idade);
+// let altura = 180;
+// console.log(altura);
+// let capa = 'dura';
+// let altura = 30;
+// let largura = 14;
+// let paginas = 96;
+
+// // objeto:
+
+// let caderno = {
+//     capa: 'dura',
+//     altura: 30,
+//     largura: 14,
+//     paginas: 96
+// }
+
+// console.log(caderno);
+
+// Arrays
+
+// let Oliver = [21, 'Qn 15', 65]
+
+//fuctions
+// let siteTheme = " "
+// let none = ""
+// function changeTheme(intensity,theme){
+//     siteTheme = intensity + " " + theme
+
+// };
+
+// console.log(siteTheme);
+// changeTheme("soft","light");
+// console.log(siteTheme)
+// changeTheme(none , "dark")
+// console.log(siteTheme)
+
+// //Operador lógico AND (&&)
+// let maiorDeIdade = true
+// let possuiCarteiraDeTrabalhor = true
+// let podeAplicar = maiorDeIdade && possuiCarteiraDeTrabalhor
+
+// console.log(podeAplicar);
+
+// //Operador lógico OR (||)
+let maiorDeIdade = true
+let possuiCarteiraDeTrabalhor = false
+let podeAplicar = maiorDeIdade || possuiCarteiraDeTrabalhor
+
+console.log(podeAplicar)
+
+// //Operador lógico NOT (!)
+let candidatoReprovado = !podeAplicar
+console.log(candidatoReprovado)
